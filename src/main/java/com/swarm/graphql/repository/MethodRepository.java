4aaab90a8e20aaf72998b46b695c118b920d2470
Alexis Colin <alexis.colin5@orange.fr>
2019-03-25 17:29:00 -0400

    Ajout de la méthode "getStartingMethods"

--- src/main/java/com/swarm/graphql/repository/MethodRepository.java
+++ src/main/java/com/swarm/graphql/repository/MethodRepository.java
@@ -7,10 +7,15 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import com.swarm.graphql.model.Method;
+import com.swarm.graphql.model.Session;
 
 public interface MethodRepository extends CrudRepository<Method, Long> {
 	
 	@Query("Select m From Method m Where m.type.id = :typeId")
 	List<Method> findByTypeId(@Param("typeId") Long typeId);
+	
+	@Query("select m from Method m, Type t, Session s where m.type = t and t.session = s and s = :session " +
+			"and m not in (select i.invoked from Invocation i group by i.invoked) order by m.id")
+	List<Method> getStartingMethods(@Param("session") Session session);
 
 }

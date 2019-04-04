3ec607e15923d57e10faea2b13d3ad1a4342a02c
Berthier <a.berthier@numericable.fr>
2019-04-04 13:36:54 -0400

    Test QueryType

---
+++ src/test/java/com/swarm/graphql/resolver/query/QueryTaskTest.java
@@ -0,0 +1,14 @@
+package com.swarm.graphql.resolver.query;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class QueryTaskTest {
+
+	@Test
+	public final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ src/test/java/com/swarm/graphql/resolver/query/QueryTypeTest.java
@@ -0,0 +1,37 @@
+package com.swarm.graphql.resolver.query;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.swarm.graphql.SwarmGraphQlApplication;
+
+import outils.JsonReader;
+
+public class QueryTypeTest {
+
+	@Before
+	public final void lancement() {
+		new SwarmGraphQlApplication();
+		SwarmGraphQlApplication.main(new String[0]);
+	}
+	
+	
+	@Test
+	public final void typesBySessionIdtest() throws IOException, JSONException {
+		
+		JSONObject json = JsonReader.readJsonFromUrl("http://localhost:8080/graphql?query=%7BtypesBySessionId(sessionId:1)%7Bid%7D%7D");
+		JSONObject data = json.getJSONObject("data");
+		
+		String valOptenuCorect =  "{\"typesBySessionId\":[{\"id\":\"1\"}]}";
+		
+		assertTrue(valOptenuCorect.equals(data.toString()));
+		
+	}
+
+}
